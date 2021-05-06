3d759a9106f8bbfe0d1eec9298f709696fe62f08
jonat <jonathan.rendon@gmail.com>
2021-05-05 20:36:14 -0400

    Actualizar skills

--- components/Layout.js
+++ components/Layout.js
@@ -45,7 +45,7 @@ const Layout = ({ children, title, footer = true, dark = false,navBar }) => {
       {footer && (
         <footer className="bg-dark text-light text-center">
           <div className="container p-4">
-            <h3>&copy; Jonathan Rendón Portfolio</h3>
+            <h3>&copy; Jonathan Rendón</h3>
             <p>2007 - {new Date().getFullYear()}</p>
             <p>All rights Reserved.</p>
           </div>
