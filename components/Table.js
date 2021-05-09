1be236d3f2fed9ff7c532c68930d7b229a91b020
jonat <jonathan.rendon@gmail.com>
2021-05-09 01:39:08 -0400

    Add Covid Cases Datatable

--- components/Table.js
+++ components/Table.js
@@ -1,7 +1,34 @@
 import React, {useState,useEffect} from 'react';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import {BootstrapTable, TableHeaderColumn,ExportCSVButton,SearchField  } from 'react-bootstrap-table';
+
+
 
 export default function Table(props) {
+   const handleExportCSVButtonClick = (onClick) => {
+        // Custom your onClick event here,
+        // it's not necessary to implement this function if you have no any process before onClick
+        console.log('This is my custom function for ExportCSVButton click event');
+        onClick();
+    }
+  const  createCustomExportCSVButton = (onClick) => {
+        return (
+            <ExportCSVButton
+                btnText='Down CSV'
+                onClick={ () => handleExportCSVButtonClick(onClick) }/>
+        );
+    }
+    const createCustomSearchField = (props) => {
+        return (
+            <SearchField
+                className='my-custom-class'
+                defaultValue=''
+                placeholder='Input a country name'/>
+        );
+    }
+    const options = {
+        exportCSVBtn: createCustomExportCSVButton,
+        searchField: createCustomSearchField
+    };
     const [data, setData] = useState([]);
 
     const [day,setDay]=useState()
@@ -24,6 +51,8 @@ export default function Table(props) {
 
                 })
                 setData(newData)
+                setDay(data.Date)
+
 
 
             });
@@ -36,17 +65,18 @@ export default function Table(props) {
         <div className="card border-secondary mb-2 animate__animated animate__fadeInUp animate__slower"
              style={{minHeight: 550}}>
             <div className="card-header text-white">Covid 19 cases by countries</div>
-            <div className="card-body text-center">
+            <div className="card-body">
+                <h3 className="text-white">Date:{day}</h3>
                 <BootstrapTable
                     data={data}
-                    pagination striped hover>
-                    <TableHeaderColumn dataField='country' isKey>Country</TableHeaderColumn>
-                    <TableHeaderColumn dataField='nconfirmed'>New Confirmed</TableHeaderColumn>
-                    <TableHeaderColumn dataField='ndeaths'>New Deaths</TableHeaderColumn>
-                    <TableHeaderColumn dataField='nrecovered'>New Recovered</TableHeaderColumn>
-                    <TableHeaderColumn dataField='tconfirmed'>Total Confirmed</TableHeaderColumn>
-                    <TableHeaderColumn dataField='tdeaths'>Total Deaths</TableHeaderColumn>
-                    <TableHeaderColumn dataField='trecovered'>Total Recovered</TableHeaderColumn>
+                    pagination striped hover options={options} exportCSV search >
+                    <TableHeaderColumn className="text-white" dataField='country' isKey>Country</TableHeaderColumn>
+                    <TableHeaderColumn className="text-white" dataField='nconfirmed'>New Confirmed</TableHeaderColumn>
+                    <TableHeaderColumn className="text-white" dataField='ndeaths'>New Deaths</TableHeaderColumn>
+                    <TableHeaderColumn className="text-white" dataField='nrecovered'>New Recovered</TableHeaderColumn>
+                    <TableHeaderColumn className="text-white" dataField='tconfirmed'>Total Confirmed</TableHeaderColumn>
+                    <TableHeaderColumn className="text-white" dataField='tdeaths'>Total Deaths</TableHeaderColumn>
+                    <TableHeaderColumn className="text-white" dataField='trecovered'>Total Recovered</TableHeaderColumn>
                 </BootstrapTable>
             </div>
         </div>
